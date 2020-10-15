0d897966716bc41fc6b486fd152d94c62bb587dc
Ranjith Aruchamy <Ranjith.Aruchamy@ATMECS.CORP>
2020-10-15 22:13:37 +0530

    created&updatedAt pwd(salt&hash) userId(OXF-D-RA-)

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,16 +1,53 @@
 const mongoose = require('mongoose');
 const User = require('../models/user')
+const bcrypt = require('bcryptjs');
 //const User = mongoose.model('UserMaster');
 
-module.exports.register = (req, res, next) => {
-    var user = new User();
-    user.firstName = req.body.firstName;
-    user.lastName = req.body.lastName;
-    user.phoneNumber = req.body.phoneNumber;
-    user.email = req.body.email;
-    user.password = req.body.password;
-    user.save((err, doc) => {
-        if(!err)
-        res.send(doc);
+module.exports.register = async (req, res, next) => {
+    var fName = req.body.firstName;
+    fName = fName.charAt(0);
+    var lName = req.body.lastName;
+    lName = lName.charAt(0);
+    const salt = await bcrypt.genSalt()
+    const hashPwd = await bcrypt.hash(req.body.password, salt);
+
+    User.countDocuments({}, function (err, count) { 
+        if (err){ 
+            res.status(500).send(err);
+        }else{ 
+            (new User(
+                {'userId': "OXF-D-" + fName + lName +"-"+ count,
+                    'firstName': req.body.firstName,
+                    'lastName': req.body.lastName,
+                    'email': req.body.email,
+                    'phoneNumber': req.body.phoneNumber,
+                    'password': hashPwd,
+                    'createdBy': req.body.firstName + " " + req.body.lastName
+                }))
+                .save()
+                .then(users => res.status(201).send(users))
+                .catch(error => res.status(500).send(error) )
+            
+        } 
+    });
+}
+
+module.exports.updateUser = async (req, res, next) => {
+/*     if(req.body.status == 'INACTIVE'){
+        var deletedby = req.body.firstName + " " + req.body.lastName;
+    } */
+
+    /* User.findOneAndUpdate({userId:req.params.userId}, {$set:req.body})
+    .then(function(){
+        User.findById({'userId': req.params.userId}).then(function(users){
+            res.status(200).send(users)}) 
     })
+    .catch(error => res.status(500).send(error)) */
+    
+ /*    User.findOne({userId:req.params.userId})
+    .then(users => res.send(users))
+    User.updateOne({userId:req.params.userId},{$set:req.body})
+    .then(users => res.send(users))
+    User.findOne({userId:req.params.userId})
+    .then(users => res.send(users)) */
 }
--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,43 +1,30 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-var userSchema = new mongoose.Schema({
-    /* userId:{
-        type:String,    
-        unique:true
-    }, */
-    status:{
-        type: String,
-        default: "ACTIVE"
-    },
-    role:{
-        type: String,
-        default:"USER"
-    },
-    firstName:{
-        type:String,
-        required:true
-    },
-    lastName:{
-        type: String,
-        required: true
-    },
-    phoneNumber:{
-        type: Number,
-        required:true
-    },
-    email:{
-        type:String,
-        required: true,
-        unique: true
-    },
-    password:{
-        type:String,
-        required:true
-    },
-    saltSecret:String
+var userSchema = new mongoose.Schema(
+  {
+    userId: { type: String, unique: true },
+    status: { type: String, default: "ACTIVE" },
+    role: { type: String, default: "USER" },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    phoneNumber: { type: Number, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    deletedAt: { type: Date },
+    createdBy:{type:String},
+    updatedBy:{type:String},
+    deletedBy:{type:String},
+    dummyA:{type:String},
+    dummyB:{type:String},
+    dummyC:{type:String},
+    dummyD:{type:String},
+    dummyE:{type:String},
+    saltSecret: String,
+  },
+  {
+    timestamps: { createdAt: "createdAt", updatedAt: "updated_at" },
+  }
+);
 
-
-});
-
-const UserMaster = mongoose.model('UserMaster', userSchema);
+const UserMaster = mongoose.model("UserMaster", userSchema);
 module.exports = UserMaster;
